2a5f4fd6b1356bdc0df66384d70154fe2da1361a
Coury Richards <146002925+couryrr-afs@users.noreply.github.com>
2024-03-18 16:42:19 -0400

    Installed dependencies and updated cmake calls
    Signed-off-by: Coury Richards <146002925+couryrr-afs@users.noreply.github.com>

--- manager/install.sh
+++ manager/install.sh
@@ -3,3 +3,4 @@ ninja -j$(nproc) -C build install
 
 # install everestpy via cmake target from everest-framework
 ninja -C build everestpy_pip_install_dist
+ninja -C build install_everest_testing
--- manager/run-test.sh
+++ manager/run-test.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-
+pip install pytest pytest-asyncio
 pytest --everest-prefix /workspace/dist core_tests/startup_tests.py 
