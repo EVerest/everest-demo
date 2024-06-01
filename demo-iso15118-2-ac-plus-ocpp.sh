601f00d59c9629b761bc2795b9fd905665d24814
Shankari <k.shankari@ee.doe.gov>
2024-05-31 17:11:04 -0700

    reset to the known good commit *after* cloning
    This should fix
    https://github.com/EVerest/everest-demo/issues/47#issuecomment-2143117112
    
    Testing done:
    
    Made a change to copy the everest demo instead of cloning it.
    
    ```
    $ bash demo-iso15118-2-ac-plus-ocpp.sh -r /.../everest-demo -3
    ...
    HEAD is now at b990d0e fix: make lb depend on services in compose
    ...
    Patching the CSMS to enable EVerest organization
    patching file docker-compose.yml
    Patching the CSMS to enable local mo root
    patching file 'config/manager/config.toml'
    Patching the CSMS to enable local mo root
    patching file 'manager/handlers/ocpp201/authorize.go'
    Starting the CSMS
    ```

--- demo-iso15118-2-ac-plus-ocpp.sh
+++ demo-iso15118-2-ac-plus-ocpp.sh
@@ -89,15 +89,16 @@ cd "${DEMO_DIR}" || exit 1
 
 
 echo "Cloning EVerest from ${DEMO_REPO} into ${DEMO_DIR}/everest-demo"
-git clone --branch "${DEMO_BRANCH}" "${DEMO_REPO}" everest-demo
-
-git reset --hard ${CSMS_BRANCH}
+# git clone --branch "${DEMO_BRANCH}" "${DEMO_REPO}" everest-demo
+cp -r "${DEMO_REPO}" everest-demo
 
 if [[ "$DEMO_VERSION" != v1.6j ]]; then
   echo "Cloning ${CSMS} CSMS from ${CSMS_REPO} into ${DEMO_DIR}/${CSMS}-csms and starting it"
   git clone ${CSMS_REPO} ${CSMS}-csms
 
   pushd ${CSMS}-csms || exit 1
+
+  git reset --hard ${CSMS_BRANCH}
 
   # Set up CSMS
   echo "Setting up ${CSMS}"
@@ -342,7 +343,7 @@ pushd everest-demo || exit 1
 docker compose --project-name everest-ac-demo --file "${DEMO_COMPOSE_FILE_NAME}" up -d --wait
 docker cp config-sil-ocpp201-pnc.yaml  everest-ac-demo-manager-1:/ext/source/config/config-sil-ocpp201-pnc.yaml
 if [[ "$DEMO_VERSION" =~ sp2 || "$DEMO_VERSION" =~ sp3 ]]; then
-  docker cp manager/cached_certs_correct_name_emaid.tar.gz everest-ac-demo-manager-1:/workspace/
+  docker cp manager/cached_certs_correct_name_emaid.tar.gz everest-ac-demo-manager-1:/ext/source/
   docker exec everest-ac-demo-manager-1 /bin/bash -c "tar xf cached_certs_correct_name_emaid.tar.gz"
 
   echo "Configured everest certs, validating that the chain is set up correctly"
@@ -355,19 +356,19 @@ else
   if [[ "$DEMO_VERSION" =~ sp1 ]]; then
     echo "Copying device DB, configured to SecurityProfile: 1"
     docker cp manager/device_model_storage_maeve_sp1.db \
-      everest-ac-demo-manager-1:/workspace/dist/share/everest/modules/OCPP201/device_model_storage.db
+      everest-ac-demo-manager-1:/ext/source/dist/share/everest/modules/OCPP201/device_model_storage.db
   elif [[ "$DEMO_VERSION" =~ sp2 ]]; then
     echo "Copying device DB, configured to SecurityProfile: 2"
     docker cp manager/device_model_storage_maeve_sp2.db \
-      everest-ac-demo-manager-1:/workspace/dist/share/everest/modules/OCPP201/device_model_storage.db
+      everest-ac-demo-manager-1:/ext/source/dist/share/everest/modules/OCPP201/device_model_storage.db
   elif [[ "$DEMO_VERSION" =~ sp3 ]]; then
     echo "Copying device DB, configured to SecurityProfile: 3"
     docker cp manager/device_model_storage_maeve_sp3.db \
-      everest-ac-demo-manager-1:/workspace/dist/share/everest/modules/OCPP201/device_model_storage.db
+      everest-ac-demo-manager-1:/ext/source/dist/share/everest/modules/OCPP201/device_model_storage.db
   fi
 fi
 
 if [[ "$DEMO_VERSION" =~ v2.0.1 ]]; then
   echo "Starting software in the loop simulation"
-  docker exec everest-ac-demo-manager-1 sh /workspace/build/run-scripts/run-sil-ocpp201-pnc.sh
+  docker exec everest-ac-demo-manager-1 sh /ext/source/build/run-scripts/run-sil-ocpp201-pnc.sh
 fi
