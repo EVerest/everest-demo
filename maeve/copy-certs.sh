404cee53bed1fed322835a108fb08e1b835cff06
Shankari <k.shankari@ee.doe.gov>
2024-11-11 15:29:03 -0800

    REFACTOR STEP 4: Pull out the code to copy the certs over
    To simplify the script and make it easier to refactor.
    
    Testing done:
    - maeve starts up and the manager connects to it
    - citrineos does not start up, but that is unchanged from the
      main branch
    
    Will punt on debugging citrineos until we have rolled forward
    
    - maeve testing
    
    ```
    2024-11-11 22:43:40.693974 [INFO] ocpp:OCPP201     :: Using certificate: "/ext/source/build/dist/etc/everest/certs/client/csms/CSMS_LEAF.pem"
    2024-11-11 22:43:40.694660 [INFO] ocpp:OCPP201     :: Using key file: "/ext/source/build/dist/etc/everest/certs/client/csms/CSMS_LEAF.key"
    2024-11-11 22:43:40.701120 [INFO] evse_security:E  :: Building new certificate hierarchy!
    2024-11-11 22:43:40.780705 [INFO] evse_security:E  :: Requesting certificate file: [CSMS] file:"/ext/source/build/dist/etc/everest/certs/ca/v2g/V2G_ROOT_CA.pem"
    2024-11-11 22:43:40.737677 [DEBG] ocpp:OCPP201     :: Loading ca csms bundle to verify server certificate: /ext/source/build/dist/etc/everest/certs/ca/v2g/V2G_ROOT_CA.pem
    2024-11-11 22:43:40.823477 [INFO] evse_security:E  :: Requesting certificate file: [CSMS] file:"/ext/source/build/dist/etc/everest/certs/ca/v2g/V2G_ROOT_CA.pem"
    2024-11-11 22:43:40.909915 [INFO] evse_security:E  :: Requesting key/pair: V2G
    2024-11-11 22:43:40.913828 [INFO] evse_security:E  :: TPM Key: false
    2024-11-11 22:43:40.924600 [WARN] evse_security:E static std::string evse_security::OpenSSLSupplier::x509_get_responder_url(evse_security::X509Handle*) :: Could not retrieve OCSP Responder URL from certificate
    2024-11-11 22:43:40.927839 [INFO] ocpp:OCPP201     :: OCPP client successfully connected to TLS websocket server
    2024-11-11 22:43:40.940441 [INFO] ocpp:OCPP201     :: Received BootNotificationResponse: {
        "currentTime": "2024-11-11T22:43:40.000Z",
        "interval": 300,
        "status": "Accepted"
    }
    with messageId: 351ad980-332b-4c63-9ba4-0a1a07250928
    ```
    
    - citrine testing
    
    ```
    Cloning EVerest from /Users/kshankar/Desktop/data/joet-everest/everest-demo into /var/folders/y5/cx3cfzrd2q116myv9ly86sw1rnlmdj/T/tmp.khZGzAKt/everest-demo
    Cloning CitrineOS CSMS from https://github.com/citrineos/citrineos-core.git into /var/folders/y5/cx3cfzrd2q116myv9ly86sw1rnlmdj/T/tmp.khZGzAKt/citrineos-csms and starting it
    Cloning into 'citrineos-csms'...
    remote: Enumerating objects: 16467, done.
    remote: Counting objects: 100% (303/303), done.
    remote: Compressing objects: 100% (221/221), done.
    remote: Total 16467 (delta 139), reused 161 (delta 70), pack-reused 16164 (from 1)
    Receiving objects: 100% (16467/16467), 5.06 MiB | 2.60 MiB/s, done.
    Resolving deltas: 100% (10384/10384), done.
    /var/folders/y5/cx3cfzrd2q116myv9ly86sw1rnlmdj/T/tmp.khZGzAKt/citrineos-csms /var/folders/y5/cx3cfzrd2q116myv9ly86sw1rnlmdj/T/tmp.khZGzAKt
    Copying certs into /var/folders/y5/cx3cfzrd2q116myv9ly86sw1rnlmdj/T/tmp.khZGzAKt/citrineos-csms/Server/data/certificates
    /var/folders/y5/cx3cfzrd2q116myv9ly86sw1rnlmdj/T/tmp.khZGzAKt/citrineos-csms/Server /var/folders/y5/cx3cfzrd2q116myv9ly86sw1rnlmdj/T/tmp.khZGzAKt/citrineos-csms /var/folders/y5/cx3cfzrd2q116myv9ly86sw1rnlmdj/T/tmp.khZGzAKt
    ```
    
    Citrine failure on both main and this branch
    
    ```
    127.9 npm verbose exit 0
    127.9 npm info ok
    131.7 npm verbose exit 1
    131.7 npm verbose code 1
    ------
    failed to solve: process "/bin/sh -c npm install --workspaces --verbose && npm run compile --workspaces --verbose" did not complete successfully: exit code: 1
    Failed to start CitrineOS.
    ```
    
    Signed-off-by: Shankari <k.shankari@ee.doe.gov>

---
+++ maeve/copy-certs.sh
@@ -0,0 +1,14 @@
+  # Set up certificates for SP2 and SP3
+    echo "Copying certs into ${DEMO_DIR}/maeve-csms/config/certificates"
+    tar xf cached_certs_correct_name_emaid.tar.gz
+    cat dist/etc/everest/certs/client/csms/CSMS_LEAF.pem \
+        dist/etc/everest/certs/ca/csms/CPO_SUB_CA2.pem \
+        dist/etc/everest/certs/ca/csms/CPO_SUB_CA1.pem \
+      > config/certificates/csms.pem
+    cat dist/etc/everest/certs/ca/csms/CPO_SUB_CA2.pem \
+        dist/etc/everest/certs/ca/csms/CPO_SUB_CA1.pem \
+      > config/certificates/trust.pem
+    cp dist/etc/everest/certs/client/csms/CSMS_LEAF.key config/certificates/csms.key
+    cp dist/etc/everest/certs/ca/v2g/V2G_ROOT_CA.pem config/certificates/root-V2G-cert.pem
+    cp dist/etc/everest/certs/ca/mo/MO_ROOT_CA.pem config/certificates/root-MO-cert.pem
+
