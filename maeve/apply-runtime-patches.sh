20068a202923b9c55aeb7eb17c49a0d4f889c547
K. Shankari <shankari@damajash.org>
2024-11-14 11:59:59 -0800

    👷Initial workflow to checkout and build the CSMS (#7)
    * 👷Initial workflow to checkout and build the CSMS
    
    This can definitely be polished, but it works now so can unblock
    https://github.com/EVerest/everest-demo/issues/78
    
    Changes:
    - Add a new workflow
    - Configure it with a hardcoded set of images to build for maeve
    - Apply compile-time patches
    - Build and push, similar to the original `cicd` workflow
    
    ----- full list of changes -----
    
    * Build on pull requests to upgrade branches as well
    
    * Handle the two directories correctly
    
    Need to explicitly `cd` into `everest-demo`
    And apply patches from there
    
    * Fix repo format
    
    * Fix checked out directory format
    
    * Again change directory properly before building
    
    * Now that build works, re-tag and push
    
    * Fix invalid workflow format
    
    * Really fix the format
    
    * read the env properly (from within the demo)
    
    * Login before pushing
    
    * Switch to the build-push-action so that push to packages works
    
    https://github.com/EVerest/everest-demo/issues/78#issuecomment-2477073447
    
    * Actually push the values
    
    * Give more permissions to the token
    
    To be consistent with
    https://docs.github.com/en/packages/managing-github-packages-using-github-actions-workflows/publishing-and-installing-a-package-with-github-actions
    
    * Change the permission to "admin"
    
    So that we can really really push?!
    
    * Temporarily switch to US-JOET so we can verify that it works
    
    * Split the maeve patches into compile time and runtime
    
    And apply only the compile time patch while building the image
    
    Signed-off-by: Shankari <k.shankari@ee.doe.gov>

--- maeve/apply-runtime-patches.sh
+++ maeve/apply-runtime-patches.sh
@@ -7,9 +7,6 @@
     echo "Patching the CSMS to enable local mo root"
     patch -p1 -i ../everest-demo/maeve/maeve-csms-local-mo-root.patch
 
-    echo "Patching the CSMS to enable local mo root"
-    patch -p1 -i ../everest-demo/maeve/maeve-csms-ignore-ocsp.patch
-
   else
     echo "Patching the CSMS to disable WSS"
     patch -p1 -i ../everest-demo/maeve/maeve-csms-no-wss.patch
