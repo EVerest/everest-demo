533e6642b54f13a3992c371be47a4103389dfcc2
Shankari <k.shankari@driveelectric.gov>
2025-01-14 17:28:29 -0800

    🔧 Change the default EnergyTransfer mode to be single phase
    This will allow us to test against cars in the the US.
    US has 1-phase, EU has 3-phase.
    
    We found, during Testival, that sending "three phase" here would break the
    handshake in many situations.
    
    Ideally, this would be read from the hw capability.
    In particular, something like this
    https://github.com/EVerest/everest-core/blob/50d8bacaab06dea0b5a58f5efd212ff1d25de239/modules/EvseManager/EvseManager.cpp#L743
    
    ```
    r_bsp->subscribe_ac_nr_of_phases_available([this](int n) { signalNrOfPhasesAvailable(n); });
    ```
    
    But that variable is computed from the min and max phase in the HardwareCapabilities
    https://github.com/EVerest/everest-core/blob/50d8bacaab06dea0b5a58f5efd212ff1d25de239/modules/EvseManager/EvseManager.cpp#L134C1-L142C56
    
    ```
        r_bsp->subscribe_capabilities([this](types::evse_board_support::HardwareCapabilities c) {
            if (ac_nr_phases_active == 0) {
                ac_nr_phases_active = c.max_phase_count_import;
            }
    
            if (ac_nr_phases_active > c.max_phase_count_import) {
                ac_nr_phases_active = c.min_phase_count_import;
            }
    
            signalNrOfPhasesAvailable(ac_nr_phases_active);
    ```
    
    the micromegawatt BSP defines the min and max phase as 1 and 3 respectively
    https://github.com/EVerest/everest-core/blob/3401718254fc6ac2c6e741c5b9db83b5bbc80dc6/modules/MicroMegaWattBSP/board_support/evse_board_supportImpl.cpp#L49
    
    ```
            caps.min_phase_count_import = 1;
            caps.max_phase_count_import = 3;
    ```
    
    so we wouldn't be able to change this from a config file anyway
    
    This would ideally be the location to get it from, but not sure how we can see that in the EvseManager.
    
    ```
      grid_connection_point:
        config_module:
          fuse_limit_A: 40
          phase_count: 3
    ```
    
    Testing done:
    - after the change, started a charge session from the simulator
    
    ```
    2025-01-15 00:21:15.790874 [INFO] evse_manager_1:  :: EVSE ISO V2G ServiceDiscoveryRes
    2025-01-15 00:21:15.900073 [DEBG] iso15118_car    pybind11_init_everestpy(pybind11::module_&)::<lambda(const std::string&)> :: Decoded message (ns=Namespace.ISO_V2_MSG_DEF): {"V2G_Message":{"Header":{"SessionID":"C6BFDFDEAC8B3727"},"Body":{"ServiceDiscoveryRes":{"ResponseCode":"OK","PaymentOptionList":{"PaymentOption":["ExternalPayment","Contract"]},"ChargeService":{"ServiceID":1,"ServiceCategory":"EVCharging","FreeService":false,"SupportedEnergyTransferMode":{"EnergyTransferMode":["AC_single_phase_core"]}},"ServiceList":{"Service":[{"ServiceID":2,"ServiceName":"Certificate","ServiceCategory":"ContractCertificate","FreeService":true}]}}}}}
    ```
    
    Signed-off-by: Shankari <k.shankari@driveelectric.gov>

--- manager/demo-patch-scripts/apply-compile-patches.sh
+++ manager/demo-patch-scripts/apply-compile-patches.sh
@@ -4,3 +4,4 @@ echo "Applying compile-time patches"
 
 cd / && patch -p0 -i /tmp/demo-patches/enable_iso_dt.patch
 cd / && patch -p1 -i /tmp/demo-patches/composite_schedule_fixes.patch
+cd / && patch -p1 -i /tmp/demo-patches/change_transfer_mode_to_single_phase.patch
