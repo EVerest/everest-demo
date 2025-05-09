e7d8e745b14b18406e4f3c0f0e60912303698a72
Shankari <k.shankari@driveelectric.gov>
2025-05-08 23:11:06 -0700

    🔧 Switch to single phase AC for the demo
    Since we will use this in the US Testival, where we expect to have cars that
    are set up to accept single phase.
    
    - Configure the hardcoded value in the EVSE
    - Change the config to support single phase in the car (EV)
    
    Signed-off-by: Shankari <k.shankari@driveelectric.gov>

--- manager/demo-patch-scripts/apply-compile-patches.sh
+++ manager/demo-patch-scripts/apply-compile-patches.sh
@@ -4,3 +4,4 @@ echo "Applying compile-time patches"
 
 cd / && patch -N -p0 -i /tmp/demo-patches/enable_iso_dt.patch
 cd / && patch -N -p1 -i /tmp/demo-patches/composite_schedule_fixes.patch
+cd / && patch -N -p1 -i /tmp/demo-patches/switch_to_single_phase.patch
