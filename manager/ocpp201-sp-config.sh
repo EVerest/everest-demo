2cf59b1b1634ec9ae929448018bec4b83c8ecd99
Shankari <k.shankari@driveelectric.gov>
2025-05-10 17:45:47 -0700

    🔧 Enable EIM + TLS
    - The car and EVSE have an encrypted connection
    - The car uses EIM to authenticate to the station
    - The station uses a non-encrypted connection to communicate with the CSMS
    
    Testing done:
    - Plugged car in with TLS; unplugged car; worked
    
    Signed-off-by: Shankari <k.shankari@driveelectric.gov>

--- manager/ocpp201-sp-config.sh
+++ manager/ocpp201-sp-config.sh
@@ -2,24 +2,36 @@
 echo "Applying generic OCPP configuration with ${DEMO_VERSION}, ${CSMS_SP1_BASE} and ${CHARGE_STATION_ID}"
 if [[ "$DEMO_VERSION" =~ sp1 || "$DEMO_VERSION" =~ sp2 || "$DEMO_VERSION" =~ sp3 ]]; then
     cp /tmp/config-sil-ocpp201-pnc.yaml /ext/source/config/config-sil-ocpp201-pnc.yaml
+    if grep -q "tls_active: true" /ext/source/config/config-sil-ocpp201-pnc.yaml; then
+        echo "TLS over ISO enabled, expanding certs."
+        pushd /ext/ && tar xf /tmp/cached_certs_correct_name_emaid.tar.gz
+
+        echo "Configured everest certs, validating that the chain is set up correctly"
+        openssl verify -show_chain -CAfile dist/etc/everest/certs/ca/v2g/V2G_ROOT_CA.pem --untrusted dist/etc/everest/certs/ca/csms/CPO_SUB_CA1.pem --untrusted dist/etc/everest/certs/ca/csms/CPO_SUB_CA2.pem dist/etc/everest/certs/client/csms/CSMS_LEAF.pem
+        popd
+    else
+        echo "TLS is not active in the configuration file, not configuring certs"
+    fi
     rm /ext/dist/share/everest/modules/OCPP201/component_config/custom/EVSE_2.json
     rm /ext/dist/share/everest/modules/OCPP201/component_config/custom/Connector_2_1.json
 fi
 
 if [[ "$DEMO_VERSION" =~ sp2 || "$DEMO_VERSION" =~ sp3 ]]; then
-  cp /tmp/cached_certs_correct_name_emaid.tar.gz /ext/source/build
-  pushd /ext/source/build && tar xf cached_certs_correct_name_emaid.tar.gz
+  echo "TLS over OCPP enabled, expanding certs."
+  pushd /ext/ && tar xf /tmp/cached_certs_correct_name_emaid.tar.gz
 
   echo "Configured everest certs, validating that the chain is set up correctly"
   openssl verify -show_chain -CAfile dist/etc/everest/certs/ca/v2g/V2G_ROOT_CA.pem --untrusted dist/etc/everest/certs/ca/csms/CPO_SUB_CA1.pem --untrusted dist/etc/everest/certs/ca/csms/CPO_SUB_CA2.pem dist/etc/everest/certs/client/csms/CSMS_LEAF.pem
   popd
+else
+  echo "TLS over OCPP disabled, not configuring certs"
 fi
 
 if [[ "$DEMO_VERSION" =~ sp1 ]]; then
     CSMS_SP1_URL=${CSMS_SP1_BASE}/${CHARGE_STATION_ID}
     echo "Configured to SecurityProfile: 1, disabling TLS and configuring server to ${CSMS_SP1_URL}"
     sed -i "s#ws://localhost:9000#${CSMS_SP1_URL}#" /ext/dist/share/everest/modules/OCPP201/component_config/standardized/InternalCtrlr.json
-    pushd /ext/source && patch -N -p0 -i /tmp/disable_iso_tls.patch && popd
+    # pushd /ext/source && patch -N -p0 -i /tmp/disable_iso_tls.patch && popd
 elif [[ "$DEMO_VERSION" =~ sp2 ]]; then
     CSMS_SP2_URL=${CSMS_SP2_BASE}/${CHARGE_STATION_ID}
     echo "Configured to SecurityProfile: 2, configuring server to  ${CSMS_SP2_URL}"
