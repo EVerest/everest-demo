4237a81bcc7b9b8ed7484aacfcf28879523e901b
Shankari <k.shankari@driveelectric.gov>
2025-07-22 13:05:17 -0700

    📝 Create a template for citrineos compile time changes
    To be consistent with maeve, in case we want to build our own image with
    compile-time fixes, like copying over the templates!
    
    Signed-off-by: Shankari <k.shankari@driveelectric.gov>

---
+++ citrineos/apply-compile-patches.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# This is a patch to the source code, so we need to apply it
+# before we build.
+# And there is no harm in turning off OCSP completely
+
+echo "No compile-time patches for Citrine at this time!"
