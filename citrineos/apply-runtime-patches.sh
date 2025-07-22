480bca53ee3b002e49a9a9fa15d4c56ba5fb12cd
Shankari <k.shankari@driveelectric.gov>
2025-07-22 13:02:15 -0700

    💩 Copy over the templates for the graphql and postgis databases
    These should really be part of the image, but committing this temporarily so we
    can go from working to working. This should be reverted once we are able to fix the image
    https://github.com/EVerest/everest-demo/pull/142#issuecomment-3087410521
    
    This supercedes https://github.com/US-JOET/everest-demo/commit/753e2ff46f86eb4fd672e9ce82b0d3e53f355812
    in which we tried to commit the files, since that seems to mess up the permissions, *and*
    ends up making the git history super large
    
    Signed-off-by: Shankari <k.shankari@driveelectric.gov>

--- citrineos/apply-runtime-patches.sh
+++ citrineos/apply-runtime-patches.sh
@@ -3,3 +3,8 @@
 export CSMS_SP1_BASE="ws://host.docker.internal:8082"
 export CSMS_SP2_BASE="wss://host.docker.internal:8443"
 export CSMS_SP3_BASE="wss://host.docker.internal:8444"
+
+git clone https://github.com/citrineos/citrineos-core.git to-copy-templates
+cp -r to-copy-templates/Server/data .
+cp -r to-copy-templates/Server/hasura-metadata .
+rm -rf to-copy-templates
