7cbe6f4e092c50198595a87f1d10a172c68ffec3
Aria Nolan <aria@chytrid.org>
2025-08-11 10:37:02 -0400

    Disable payment patches
    Signed-off-by: Aria Nolan <aria@chytrid.org>
    Signed-off-by: Abby Wheelis <Abigail.Wheelis@nrel.gov>

--- manager/demo-patch-scripts/apply-runtime-patches.sh
+++ manager/demo-patch-scripts/apply-runtime-patches.sh
@@ -3,10 +3,10 @@
 echo "Applying iso15118_prevent_m1_crash.patch"
 cd / && patch -p0 -N -i /tmp/demo-patches/iso15118_prevent_m1_crash.patch
 
-echo "Applying enabled_payment_method_in_python.patch"
-cd /ext && patch -N -p0 -i /tmp/demo-patches/enable_payment_method_in_python.patch
-echo "Applying support_payment_in_jsevmanager.patch"
-cd /ext/dist/libexec/everest && patch -N -p1 -i /tmp/demo-patches/support_payment_in_jsevmanager.patch
+# echo "Applying enabled_payment_method_in_python.patch"
+# cd /ext && patch -N -p0 -i /tmp/demo-patches/enable_payment_method_in_python.patch
+# echo "Applying support_payment_in_jsevmanager.patch"
+# cd /ext/dist/libexec/everest && patch -N -p1 -i /tmp/demo-patches/support_payment_in_jsevmanager.patch
 echo "Applying hw_cap_down_to_16A.patch"
 cd / && patch -N -p0 -i /tmp/demo-patches/hw_cap_down_to_16A.patch
 
