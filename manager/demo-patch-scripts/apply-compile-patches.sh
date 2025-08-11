fb74b1b399c070751dc675f31e2d6f0fea789bbd
Aria Nolan <aria@chytrid.org>
2025-08-11 16:38:01 -0400

    Fix patches for everest
    Signed-off-by: Aria Nolan <aria@chytrid.org>
    Signed-off-by: Abby Wheelis <Abigail.Wheelis@nrel.gov>

--- manager/demo-patch-scripts/apply-compile-patches.sh
+++ manager/demo-patch-scripts/apply-compile-patches.sh
@@ -3,7 +3,7 @@
 echo "Applying compile-time patches"
 
 cd / && patch -N -p0 -i /tmp/demo-patches/departure-time-pr.patch
-cd / && patch -N -p0 -i /tmp/demo-patches/enable_iso_dt.patch
+# cd / && patch -N -p0 -i /tmp/demo-patches/enable_iso_dt.patch
 cd / && patch -N -p1 -i /tmp/demo-patches/composite_schedule_fixes.patch
 cd / && patch -N -p1 -i /tmp/demo-patches/switch_to_single_phase.patch
 cd / && patch -N -p0 -i /tmp/demo-patches/esdp.patch
