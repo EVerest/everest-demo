c0711eed99793de379caf3433726b45ad56ece9b
Aria Nolan <aria@chytrid.org>
2025-08-21 11:06:54 -0400

    add compile step to build script
    Signed-off-by: Aria Nolan <aria@chytrid.org>

--- custom-yocto-build/build-image.sh
+++ custom-yocto-build/build-image.sh
@@ -7,12 +7,13 @@ usage="
 	          build for the Micro Mega Watt Charger
 "
 
-MACHINE="umwc"
+MACHINE="arm64vm"
 
 while getopts ':cnh' option; do
 	case "$option" in
 		n)  NREL_CERTS=1 ;;
 		c)  CUSTOM_CERTS="$OPTARG" ;;
+		m)  MACHINE="$OPTARG" ;;
 		h)  echo -e "$usage"; exit ;;
 		\?) echo -e "illegal option: -$OPTARG\n" >&2
 		    echo -e "$usage" >&2
@@ -22,9 +23,9 @@ done
 
 docker compose -f docker-compose.kas.yml up -d
 
-if [ -n $NREL_CERTS ]; then
+if [ -n "$NREL_CERTS" ]; then
 	echo "installing root certs for NREL"
-	docker exec --user root custom-yocto-build-kas-1 bash -c \
+	docker exec --user root everest-kas bash -c \
 		"curl -fsSLk -o /usr/local/share/ca-certificates/nrel_root.crt \
 		https://raw.github.nrel.gov/TADA/nrel-certs/v20180329/certs/nrel_root.pem && \
 		curl -fsSLk -o /usr/local/share/ca-certificates/nrel_xca1.crt \
@@ -32,14 +33,18 @@ if [ -n $NREL_CERTS ]; then
 		update-ca-certificates"
 fi
 
-if [ -n $CUSTOM_CERTS ]; then
+if [ -n "$CUSTOM_CERTS" ]; then
 	echo "installing custom certs"
-	docker cp $CUSTOM_CERTS/* custom-yocto-build-kas-1:/usr/local/share/ca-certificates
-	docker exec --user root custom-yocto-build-kas-1 update-ca-certificates
+	docker cp $CUSTOM_CERTS/* everest-kas:/usr/local/share/ca-certificates
+	docker exec --user root everest-kas update-ca-certificates
 fi
 
-docker exec --user root custom-yocto-build-kas-1 mkdir -p /usr/include/python3.11
-docker exec --user root custom-yocto-build-kas-1 chown -R builder:builder /workdir
+docker exec --user root everest-kas mkdir -p /usr/include/python3.11
+docker exec --user root everest-kas chown -R builder:builder /workdir
 
-docker compose --project-name custom-yocto-build down
+docker cp meta-everest-dev everest-kas:/builder
+
+docker exec --user builder --workdir /workdir everest-kas kas build /builder/meta-everest-dev/${MACHINE}.yml
+
+docker compose -f docker-compose.kas.yml down
 
