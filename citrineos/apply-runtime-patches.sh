9a8611772ff747d0d75ec8e5eac77257636394e1
Abby Wheelis <Abigail.Wheelis@nrel.gov>
2025-11-04 10:53:21 -0700

    add hack to make metadata avaliable
    Signed-off-by: Abby Wheelis <Abigail.Wheelis@nrel.gov>

--- citrineos/apply-runtime-patches.sh
+++ citrineos/apply-runtime-patches.sh
@@ -3,3 +3,7 @@
 export CSMS_SP1_BASE="ws://host.docker.internal:8082"
 export CSMS_SP2_BASE="wss://host.docker.internal:8443"
 export CSMS_SP3_BASE="wss://host.docker.internal:8444"
+
+git clone https://github.com/citrineos/citrineos-core.git --branch v1.7.1 to-copy-templates
+cp -r to-copy-templates/Server/data .
+cp -r to-copy-templates/Server/hasura-metadata .
