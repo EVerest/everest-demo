7daabe46db5b150a681a7a21a79bd8035b4f2a17
Abby Wheelis <Abigail.Wheelis@nrel.gov>
2025-12-04 11:27:32 -0700

    undo changes to build script
    Signed-off-by: Abby Wheelis <Abigail.Wheelis@nrel.gov>

--- manager/dev-build-install-configure.sh
+++ manager/dev-build-install-configure.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 /entrypoint.sh run-script compile
-echo "ABOUT TO INSTALL"
+
 /entrypoint.sh run-script install
 
-# echo "Applying Post-Build patches..."
+echo "Applying Post-Build patches..."
 bash /tmp/demo-patch-scripts/apply-runtime-patches.sh
 
 pushd /tmp && bash ./ocpp201-sp-config.sh && popd
